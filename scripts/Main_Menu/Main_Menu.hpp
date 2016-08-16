6f9f8fe25a807a4493ac6fd5a2b7e43bd044f500
[HA]Korvet <silver.ranger.001@gmail.com>
2016-08-16 08:49:45 +0300

    Меню инструктора, фикс бага названия
    Улучшен внешний вид меню инструктора, фикс бага показа названия миссии в
    списке на сервере

--- scripts/Main_Menu/Main_Menu.hpp
+++ scripts/Main_Menu/Main_Menu.hpp
@@ -5,12 +5,10 @@ class Main_Menu
 	name=Main_Menu;
 	idd=3100;
 	movingEnable=1;
-	controlsBackground[]={BG,FR,FR1,FR2,t0};
+	controlsBackground[]={BG,FR,t0};
 	objects[]={};
 	controls[]={	
-			b01,b02,b03,b04,b05,c01,c02,
-			b11,b12,b13,b14,c11,c12,
-			b21,b22,b23,c21,c22,
+			redstar,b01,b02,b03,c01,c02,
 			bclose
 	};
 
@@ -18,8 +16,8 @@ class Main_Menu
 	class b01:RscButton
 	{
 		idc=-1;
-		x = 0.29; y = 0.08;
-		w = 0.2; h = 0.03;
+		x = 0.21; y = 0.11;
+		w = 0.3; h = 0.06;
 		text="Телепорт";
 		action="0 = [(lbCurSel 3001), MM_Team] spawn fHA_MM_Telepotr";
 	};
@@ -27,15 +25,15 @@ class Main_Menu
 	class c01:RscCombo
 	{
 		idc=3001;
-		x = 0.51; y = 0.08;
-		w = 0.2; h = 0.03;
+		x = 0.53; y = 0.11;
+		w = 0.32; h = 0.06;
 	};
 
 	class b02:RscButton
 	{
 		idc=-1;
-		x = 0.29; y = 0.12;
-		w = 0.2; h = 0.03;
+		x = 0.21; y = 0.20;
+		w = 0.3; h = 0.06;
 		text="Телепорт вертолета ПДП";
 		action="0 = [0, [desant_tr]] spawn fHA_MM_Telepotr";
 	};
@@ -43,8 +41,8 @@ class Main_Menu
 	class b03:RscButton
 	{
 		idc=-1;
-		x = 0.29; y = 0.17;
-		w = 0.2; h = 0.03;
+		x = 0.21; y = 0.29;
+		w = 0.3; h = 0.06;
 		text="Телепорт группы";
 		action="0 = [units (MM_Teams_List select (lbCurSel 3002))] spawn fHA_MM_Gr_Telepotr";
 	};
@@ -52,178 +50,59 @@ class Main_Menu
 	class c02:RscCombo
 	{
 		idc=3002;
-		x = 0.51; y = 0.17;
-		w = 0.2; h = 0.03;
+		x = 0.53; y = 0.29;
+		w = 0.32; h = 0.06;
 	};
 
-	class b04:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.21;
-		w = 0.2; h = 0.03;
-		text="Телепорт ящика";
-		//action="0 = [0, [box]] spawn fHA_MM_Telepotr; 0 = [box] execVM 'scripts\box_ammo.sqf';";
-		action="hint 'Временно не работает'";
-/*		text="Управление группами";
-		action="[] execVM 'GC_Menu\GC_Open.sqf'";
-*/	};
-
-	class b05:RscButton
-	{
-		idc=-1;
-		x = 0.63; y = 0.21;
-		w = 0.08; h = 0.03;
-		text="Ран. бот";
-		//action="0 = [] execVM 'scripts\create_uncos.sqf'";
-		action="hint 'Временно не работает'";
-/*		text="Сброс";
-		action="GC_menu_in_use = false; hint 'ВНИМАНИЕ!\n\nСброс блокировки меню управления труппами'";
-*/	};
 
 //////////////Разное////////////////////
 
-	class b11:RscButton
-	{
-		
-		x = 0.29; y = 0.38;
-		w = 0.2; h = 0.03;
-		text="Создать технику";
-		//action="closeDialog 0; 0 = [player,(getPos player),[50,5],MM_VehTypes1,'scripts\HA_coin_interface.sqf'] spawn f_HA_MM_COIN";
-		action="0 = [(lbCurSel 3101), MM_VehTypes1] spawn fHA_MM_EmptyVehGen";
-	};
-
-	class c11:RscCombo
-	{
-		idc=3101;
-		x = 0.51; y = 0.38;
-		w = 0.2; h = 0.03;
-		//text="Создать технику";
-		//action="closeDialog 0; hint 'Укажите зону для создания техники'; 0 = [player,[],[50,5],MM_VehTypes1,'scripts\HA_coin_interface.sqf'] spawn f_HA_MM_COIN";
-	};
-
-	class b12:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.42;
-		w = 0.2; h = 0.03;
-		text="Техника с парашютом";
-		//action="0 = [(lbCurSel 3101), MM_VehTypes1] execVM 'scripts\create_inAir.sqf'";
-		action="hint 'Временно не работает'";
-	};
-
-	class b13:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.47;
-		w = 0.2; h = 0.03;
-		text="Арт. удар";
-		action="hint 'Временно не работает'";
-		//action="0 = [(lbCurSel 3102), MM_ArtyTypes] spawn fHA_MM_Artylery";
-	};
-	
-	class c12:RscCombo
-	{
-		idc=3102;
-		x = 0.51; y = 0.47;
-		w = 0.2; h = 0.03;
-	};
-
-	class b14:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.52;
-		w = 0.2; h = 0.03;
-/*		text="Время/погода";
-		action="hint 'В разработке'";
-*/	};
 
 ///////////////ИИ///////////////////
 
-	class b21:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.68;
-		w = 0.2; h = 0.03;
-		text="Создать пехоту ИИ";
-		action="0 = [(lbCurSel 3201), MM_AiInfTypesWood] spawn fHA_MM_AiInfGen";
-	};
-
-	class c21:RscCombo
-	{
-		idc=3201;
-		x = 0.51; y = 0.68;
-		w = 0.2; h = 0.03;
-	};
-
-	class b22:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.72;
-		w = 0.2; h = 0.03;
-		text="Создать технику ИИ";
-		action="0 = [(lbCurSel 3202), MM_AiArmorTypes1] spawn fHA_MM_AiArmoryGen";
-	};
-
-	class c22:RscCombo
-	{
-		idc=3202;
-		x = 0.51; y = 0.72;
-		w = 0.2; h = 0.03;
-	};
-
-	class b23:RscButton
-	{
-		idc=-1;
-		x = 0.29; y = 0.77;
-		w = 0.2; h = 0.03;
-		text="Удалить группу ИИ";
-		action="0 = [] spawn fHA_MM_DelAiGroopsInit";
-	};
 
 ///////////////Оформление//////////////////
 	class BG:RscBackground
 	{
 		idc = -1;
-		x = 0.28; y = 0;
-		w = 0.44; h = 1;
+		x = 0.18; y = 0;
+		w = 0.69; h = 0.6;
 	};
 
 	class FR:RscBgFrame
 	{
 		idc = -1;
-		x = 0.28; y = 0;
-		w = 0.44; h = 1;
+		x = 0.18; y = 0;
+		w = 0.69; h = 0.6;
 	};
 
-	class FR1:RscBgFrame
-	{
-		idc = -1;
-		x = 0.28; y = 0.07;
-		w = 0.44; h = 0.3;
-	};
+        class redstar:MenuPicture
+        {
+         idc = -1;
+         text = "pics\redstar.paa";
 
-	class FR2:RscBgFrame
-	{
-		idc = -1;
-		x = 0.28; y = 0.67;
-		w = 0.44; h = 0.28;
-	};
+         x = 0.68;
+         y = 0.38;
+         w = 0.15;
+         h = 0.2;
+
+        };
 
 	class t0:RscText
 	{
 		idc = -1;
 		type =  CT_STATIC ;
 		sizeEx = 0.05;
-		x = 0.3; y = 0.01;
+		x = 0.32; y = 0.01;
 		w = 0.4; h = 0.05;
-		text = "Functions";
+		text = "Рабочее место инструктора";
 	};
 
 	class bclose:RscButton
 	{
 		idc=-1;
-		x = 0.29; y = 0.96;
-		w = 0.2; h = 0.03;
+		x = 0.21; y = 0.50;
+		w = 0.3; h = 0.06;
 		text="Закрыть";
 		action="closeDialog 0";
 	};
--- scripts/onPlayerStart.sqf
+++ scripts/onPlayerStart.sqf
@@ -10,63 +10,25 @@ TitleText[" ", "BLACK IN",2];
 ["www.Our-Army.su", "Тренировочный полигон"] spawn BIS_fnc_infoText;
 
 
-/*
-if (leader player == player) then {
-	if (player getVariable["bgr_tren_dolznost",0] == 0) then {
-		player setVariable["bgr_tren_dolznost",2];
-	};
-};
-*/
-
-//if (player in [zeus4,zeus5,zeus6,zeus7,zeus8,zeus9,zeus10]) then {
-/*if (player==zeus8) {
-	if (player getVariable["bgr_tren_dolznost",0] == 0) {
-		player setVariable["bgr_tren_dolznost",2];
-	};
-
-};
-*/
-
 
 if (player getVariable["bgr_tren_dolznost",0] > 0) then {
 
-	//_acm = _groupLogic createUnit ["AmbientCombatManager",position player,[],0,"NONE"];
-	//_acm synchronizeObjectsAdd [player];
 
-/*	
-	if (isNil {player getVariable "bgr_HighCommand"}) then {
-		_groupLogic = createGroup sideLogic;
-		_hc = _groupLogic createUnit ["HighCommand",position player,[],0,"NONE"];
-		_hcs = _groupLogic createUnit ["HighCommandSubordinate",position player,[],0,"NONE"];
-		
-		_hc synchronizeObjectsAdd [player,_hcs];
-		_hcs synchronizeObjectsAdd [_hc];
-		player setVariable ["bgr_HighCommand", 1, true];		
-	};
-*/
-
-// 0 = [["Меню", "scripts\Main_Menu\MM_Open.sqf"]] call CBA_fnc_addPlayerAction;
-
-
-_statement = {
+  _statement = {
     [] execVM "scripts\Main_Menu\MM_Open.sqf";
-};
+  };
 
-_action = ["HA_Tren","Тренировка","",{hint '';},{true}] call ace_interact_menu_fnc_createAction;
-[ace_player, 1, ["ACE_SelfActions"], _action] call ace_interact_menu_fnc_addActionToObject;
+  _action = ["HA_Tren","Инструктор","",{hint '';},{true}] call ace_interact_menu_fnc_createAction;
+  [ace_player, 1, ["ACE_SelfActions"], _action] call ace_interact_menu_fnc_addActionToObject;
 
-_action = ["HA_Menu","Меню","",_statement,{true}] call ace_interact_menu_fnc_createAction;
-[ace_player, 1, ["ACE_SelfActions","HA_Tren"], _action] call ace_interact_menu_fnc_addActionToObject;
-
+  _action = ["HA_Menu","Меню","",_statement,{true}] call ace_interact_menu_fnc_createAction;
+  [ace_player, 1, ["ACE_SelfActions","HA_Tren"], _action] call ace_interact_menu_fnc_addActionToObject;
 
 };
 
 
 
 if (player getVariable["bgr_tren_dolznost",0] == 1) then {
-  //0 = [["Спектатор(выход на F10)", "scripts\ace_spect.sqf"]] call CBA_fnc_addPlayerAction;
-  //0 = [["<t color='#00ff00'>Наблюдение</t>", "spectator.sqf"]] call CBA_fnc_addPlayerAction;
-  //0 = [["Zeus(импорт/экспорт)", clientFolder+"\ZeusSaver_Open.sqf"]] call CBA_fnc_addPlayerAction;
 
   _spectator = {
       [] execVM "spectator.sqf";
@@ -75,7 +37,6 @@ if (player getVariable["bgr_tren_dolznost",0] == 1) then {
   _zeus_imex = {
       _zpath = clientFolder+"\ZeusSaver_Open.sqf";
       [] execVM _zpath;
-      //hint format["-%1-",_zpath];
   };
 
   _action = ["HA_Spectator","Наблюдение","",_spectator,{true}] call ace_interact_menu_fnc_createAction;
@@ -85,6 +46,5 @@ if (player getVariable["bgr_tren_dolznost",0] == 1) then {
   [ace_player, 1, ["ACE_SelfActions","HA_Tren"], _action] call ace_interact_menu_fnc_addActionToObject;
 	
 };
-
 
 player switchMove "AmovPercMstpSlowWrflDnon";  //lower players weapon
